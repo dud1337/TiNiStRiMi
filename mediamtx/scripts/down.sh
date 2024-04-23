9969903f433a93a84a9042eb7c5cce36b945b276
dud1337 <grant@dud.li>
2024-04-23 13:56:52 +0200

    Dstream

--- mediamtx/scripts/down.sh
+++ mediamtx/scripts/down.sh
@@ -4,3 +4,6 @@ if [ -n "$2" ] && [ "$2" = "ondemand" ]; then
 fi
 
 echo -n "{\"status\":\"offline\"}" > /shared/status.json
+if [ -n "$OPSDROID_AUTH_TOKEN" ]; then
+    WGET --HEader="Authorization: Bearer $OPSDROID_AUTH_TOKEN" --post-data='{"stream_state_change":"stop"}' -O /dev/null "$OPSDROID_STREAM_ENDPOINT/skill/TiNiStRiMi-opsdroid/update"
+fi
