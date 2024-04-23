37caefb6faeda6283a3cc77f52b9ebd7bec0410d
dud1337 <grant@dud.li>
2024-04-23 14:51:38 +0200

    opsdroid functionality

--- scripts/down.sh
+++ scripts/down.sh
@@ -4,3 +4,7 @@ if [ -n "$2" ] && [ "$2" = "ondemand" ]; then
 fi
 
 echo -n "{\"status\":\"offline\"}" > /usr/share/nginx/html/status.json
+
+if [ -n "$OPSDROID_AUTH_TOKEN" ]; then
+        WGET --HEader="Authorization: Bearer $OPSDROID_AUTH_TOKEN" --post-data='{"stream_state_change":"stop"}' -O /dev/null "$OPSDROID_STREAM_ENDPOINT/skill/TiNiStRiMi-opsdroid/update"
+fi
--- scripts/up.sh
+++ scripts/up.sh
@@ -5,3 +5,7 @@ if [ -n "$2" ] && [ "$2" = "ondemand" ]; then
 fi
 
 echo -n "{\"status\":\"online\",\"type\":\"$1\"}" > /usr/share/nginx/html/status.json
+
+if [ -n "$OPSDROID_AUTH_TOKEN" ]; then
+        wget --header="Authorization: Bearer $OPSDROID_AUTH_TOKEN" --post-data='{"stream_state_change":"start"}' -O /dev/null "$OPSDROID_STREAM_ENDPOINT/skill/TiNiStRiMi-opsdroid/update"
+fi
